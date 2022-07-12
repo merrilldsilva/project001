de43519f4585e3d22f476151ec03cf00792fe4f9
TheDoctor <merrilldsilva@gmail.com>
2022-07-12 14:33:18 +0530

    script

--- app/js/script.js
+++ app/js/script.js
@@ -1,1 +1,14 @@
-console.log('hello world');
+const btnh = document.querySelector('#btnham');
+btnh.addEventListener('click',function(){
+    console.log('click initiated');
+
+    if(btnh.classList.contains('open')){
+
+        btnh.classList.remove('open');
+    }
+
+    else{
+        btnh.classList.add('open');
+    }
+
+});
