3351594968030692b116f4584b15c4895dda13be
TheDoctor <merrilldsilva@gmail.com>
2022-07-22 17:13:37 +0530

    overlay

--- app/js/script.js
+++ app/js/script.js
@@ -1,14 +1,17 @@
 const btn = document.querySelector('#btnham');
+const over = document.querySelector('#over');
 btn.addEventListener('click',function(){
     console.log('click initiated');
-
+    
     if(btn.classList.contains('open')){
 
         btn.classList.remove('open');
+        over.classList.remove('overlay');
     }
 
     else{
         btn.classList.add('open');
+        over.classList.add('overlay');
     }
 
 });
